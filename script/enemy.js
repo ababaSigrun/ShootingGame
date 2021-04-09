d329230fbafe64c33b641df7dcc53f72ae61a7e3
ababaSigrun <ababa715git@gmail.com>
2021-04-09 07:59:49 +0000

    敵機の出現判定を記載

--- script/enemy.js
+++ script/enemy.js
@@ -16,16 +16,36 @@ class Enemy {
     }
 }
 
+
+
+
 /**
  * 敵機計算
  */
 function enemyCal() {
-    console.log("enemyCal");
+    var random = Math.random();
+    var color = "none";
+    // 出現判定。
+    if (random <= 0.98) {
+        // 敵出現なし
+        console.log(color);
+    } else if (random > 0.98 && random <= 0.99) {
+        // 黄色出現
+        color = "yellow";
+        console.log(color);
+    } else if (random > 0.99 && random <= 0.999) {
+        // 緑出現
+        color= "Green";
+        console.log(color);
+    } else if (random > 0.999) {
+        color = "Pink";
+        console.log(color);
+    }
 }
 
 /**
  * 敵機描写
  */
 function enemyDrow() {
-    console.log("enemyDrow");
+    // console.log("enemyDrow");
 }
